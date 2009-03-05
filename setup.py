e9387e9419f2e46d8ae788a43eb5a5b3bc5485e4
unknown <Oscar@.(none)>
2009-03-06 00:55:32 +0100

    Make single file executable from setup.exe

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
 
-setup(console=['sisyfos.py'])
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1}},
+    windows = [{'script': "sisyfos.py"}],
+    zipfile = None,
+)
+
